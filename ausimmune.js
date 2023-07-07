3ea033f9ba486299fc52b75711e77d7803c9313e
edgiest1 <121899403+edgiest1@users.noreply.github.com>
2023-07-07 21:01:03 +1000

    Update ausimmune.js

--- ausimmune.js
+++ ausimmune.js
@@ -1,1 +1,14 @@
-document.write("Javascript test");
+prompt("Factorial of ... ?", "2");
+let array = [];
+const factorial = fact(n) {
+  if (n === 0) {
+    return 1;
+    for (let divisor=1; divisor<n/divisor; divisor+=1) {
+      if (n%divisor===0) {
+        return false;
+      }
+    array = array.unPush(n);
+    }
+  }
+}
+document.write(array);
